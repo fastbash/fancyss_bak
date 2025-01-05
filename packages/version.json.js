08d0f65935622b60bf236b9c0b9bea0f20c9b755
fastbash <yovfal@gmail.com>
2025-01-06 01:23:22 +0800

    3.3.6

---
+++ packages/version.json.js
@@ -0,0 +1,14 @@
+{
+  "name": "fancyss",
+  "version": "3.3.5",
+  "md5_hnd_full": "c7e45eaf7ce9b0eaf936a15d888da2f6",
+  "md5_hnd_lite": "d1f002d8b11d28a84a3b9937b4f4bd1f",
+  "md5_hnd_v8_full": "9209b6e3eb6f6936056761a450f80c4c",
+  "md5_hnd_v8_lite": "a8cb52ef8e39d44458399e99206a9f5f",
+  "md5_qca_full": "e022fcb152cd20272351e450a2da2e91",
+  "md5_qca_lite": "6706a074357b2d387de6819db1622e70",
+  "md5_arm_full": "4540acdf443ec02e3edc332bf6261ffd",
+  "md5_arm_lite": "2d25572a3d8f4c813033e26105223ae9",
+  "md5_mtk_full": "349803d92281fc492840aed9ee0a18f3",
+  "md5_mtk_lite": "77452c73b5b315c7ad76b4dc3c0f4827"
+}
